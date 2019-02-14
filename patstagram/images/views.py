3bf5efd0a2e2e64de47ab6db867d796deecd02f5
JaeSukHwang <dajasin245@naver.com>
2019-02-14 23:10:40 +0900

    toggle like unlike

--- patstagram/images/views.py
+++ patstagram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 
@@ -38,13 +39,22 @@ class LikeImage(APIView):
 
         try:
             found_image = models.Image.objects.get(id=image_id)
-        except:
-            return Response(status=404)
-        
-        new_like = models.Like.objects.create(
-            creator=user,
-            image=found_image
-        )
-        new_like.save()
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisting_like.delete()
 
-        return Response(status=200)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        except models.Like.DoesNotExist:
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)
