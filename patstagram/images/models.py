2124a1c9b03434119ad3a0c4fd6a5b6bdeaf73e7
JaeSukHwang <dajasin245@naver.com>
2019-02-14 13:22:22 +0900

    image, commment, like models and relation

--- patstagram/images/models.py
+++ patstagram/images/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from patstagram.users import models as user_models
 
 # Create your models here.
+
+class TimeStampModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+
+class Image(TimeStampModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+
+class Comment(TimeStampModel):
+
+    message = models.TextField()  
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+class Like(TimeStampModel):
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
