9d0265c1a2df6f0e70a8332db566495a8b95631c
JaeSukHwang <dajasin245@naver.com>
2019-02-14 22:16:13 +0900

    url variable use

--- patstagram/images/urls.py
+++ patstagram/images/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "images"
 urlpatterns = [
-    path("", view=views.Feed.as_view(), name="all_images"),
+    path("", view=views.Feed.as_view(), name="feed"),
+    re_path(r'(?P<image_id>\w+)/like/', view=views.LikeImage.as_view(), name="like_image")
 ]
 
