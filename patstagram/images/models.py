31353b42c5e78b4362abd4887e633a9410918b5e
JaeSukHwang <dajasin245@naver.com>
2019-02-14 18:27:14 +0900

    related_name designated

--- patstagram/images/models.py
+++ patstagram/images/models.py
@@ -26,7 +26,7 @@ class Comment(TimeStampModel):
 
     message = models.TextField()  
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name="comments")
 
     def __str__(self):
         return self.message
@@ -34,7 +34,7 @@ class Comment(TimeStampModel):
 class Like(TimeStampModel):
 
     creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name="likes")
 
     def __str__(self):
         return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
