78500ef264a616f6ddfe78b7c149c841bf37d538
JaeSukHwang <dajasin245@naver.com>
2019-02-15 12:09:21 +0900

    comment delete

--- patstagram/images/urls.py
+++ patstagram/images/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    re_path(r'(?P<image_id>\w+)/like/', view=views.LikeImage.as_view(), name="like_image"),
-    re_path(r'(?P<image_id>\w+)/comment/', view=views.CommentOnImage.as_view(), name="comment_image"),
+    re_path(r'^(?P<image_id>[0-9]+)/likes/$', view=views.LikeImage.as_view(), name="like_image"),
+    re_path(r'^(?P<image_id>[0-9]+)/comments/$', view=views.CommentOnImage.as_view(), name="comment_image"),
+    re_path(r'^comments/(?P<comment_id>[0-9]+)/$', view=views.Comment.as_view(), name="comment"),
 ]
 
