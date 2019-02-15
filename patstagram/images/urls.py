4f3513f35ca25721d491f2a6244d33304f63d974
JaeSukHwang <dajasin245@naver.com>
2019-02-15 11:47:49 +0900

    comment on image

--- patstagram/images/urls.py
+++ patstagram/images/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    re_path(r'(?P<image_id>\w+)/like/', view=views.LikeImage.as_view(), name="like_image")
+    re_path(r'(?P<image_id>\w+)/like/', view=views.LikeImage.as_view(), name="like_image"),
+    re_path(r'(?P<image_id>\w+)/comment/', view=views.CommentOnImage.as_view(), name="comment_image"),
 ]
 
