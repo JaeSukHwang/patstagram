0345f2ae5458abeef3cc080f107b819cd57327ca
JaeSukHwang <dajasin245@naver.com>
2019-02-16 20:41:51 +0900

    follower list

--- patstagram/users/urls.py
+++ patstagram/users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     re_path(r'^(?P<user_id>[0-9]+)/follow/$', view=views.FollowUser.as_view(), name="follow_user"),
     re_path(r'^(?P<user_id>[0-9]+)/unfollow/$', view=views.UnFollowUser.as_view(), name="unfollow_user"),
     re_path(r'^(?P<username>\w+)/$', view=views.UserProfile.as_view(), name="user_profile"),
+    re_path(r'^(?P<username>\w+)/followers/$', view=views.UserFollowers.as_view(), name="user_followers"),
+
 ]
