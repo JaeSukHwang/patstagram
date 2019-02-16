7ee78f6b622356278a7718f72249cccd1d9399f1
JaeSukHwang <dajasin245@naver.com>
2019-02-16 22:21:02 +0900

    user search

--- patstagram/users/urls.py
+++ patstagram/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("explore/", view=views.ExploreUsers.as_view(), name="explore_users"),
     re_path(r'^(?P<user_id>[0-9]+)/follow/$', view=views.FollowUser.as_view(), name="follow_user"),
     re_path(r'^(?P<user_id>[0-9]+)/unfollow/$', view=views.UnFollowUser.as_view(), name="unfollow_user"),
+    path("search/", view=views.Search.as_view(), name="search"),    
     re_path(r'^(?P<username>\w+)/$', view=views.UserProfile.as_view(), name="user_profile"),
     re_path(r'^(?P<username>\w+)/followers/$', view=views.UserFollowers.as_view(), name="user_followers"),
     re_path(r'^(?P<username>\w+)/following/$', view=views.UserFollowing.as_view(), name="user_following"),
