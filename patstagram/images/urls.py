5f7111eb93dd7b84fa9e82cd00df0e2f294118df
JaeSukHwang <dajasin245@naver.com>
2019-02-17 23:01:18 +0900

    like image

--- patstagram/images/urls.py
+++ patstagram/images/urls.py
@@ -6,8 +6,8 @@ app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     re_path(r'^(?P<image_id>[0-9]+)/$', view=views.ImageDetail.as_view(), name="image_detail"),
-    re_path(r'^(?P<image_id>[0-9]+)/like/$', view=views.LikeImage.as_view(), name="like_image"),
-    re_path(r'^(?P<image_id>[0-9]+)/unlike/$', view=views.UnLikeImage.as_view(), name="unlike_image"),
+    re_path(r'^(?P<image_id>[0-9]+)/likes/$', view=views.LikeImage.as_view(), name="like_image"),
+    re_path(r'^(?P<image_id>[0-9]+)/unlikes/$', view=views.UnLikeImage.as_view(), name="unlike_image"),
     re_path(r'^(?P<image_id>[0-9]+)/comments/$', view=views.CommentOnImage.as_view(), name="comment_image"),
     re_path(r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$', view=views.ModerateComments.as_view(), name="moderate_comment"),
     re_path(r'^comments/(?P<comment_id>[0-9]+)/$', view=views.Comment.as_view(), name="comment"),
