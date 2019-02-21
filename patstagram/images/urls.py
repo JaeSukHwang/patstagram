ed3c87549578cd97bd6cc9a33bd87a6afbbb0e9c
JaeSukHwang <dajasin245@naver.com>
2019-02-21 22:00:32 +0900

    post image

--- patstagram/images/urls.py
+++ patstagram/images/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "images"
 
 urlpatterns = [
-    path("", view=views.Feed.as_view(), name="feed"),
+    path("", view=views.Images.as_view(), name="feed"),
     re_path(r'^(?P<image_id>[0-9]+)/$', view=views.ImageDetail.as_view(), name="image_detail"),
     re_path(r'^(?P<image_id>[0-9]+)/likes/$', view=views.LikeImage.as_view(), name="like_image"),
     re_path(r'^(?P<image_id>[0-9]+)/unlikes/$', view=views.UnLikeImage.as_view(), name="unlike_image"),
