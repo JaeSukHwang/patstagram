18f54e02aa6d63190e1c4f7d0a90f4d558bded7f
JaeSukHwang <dajasin245@naver.com>
2019-02-21 18:55:58 +0900

    add tagfield in image

--- patstagram/images/serializers.py
+++ patstagram/images/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from . import models
 from patstagram.users import models as user_models
+from taggit_serializer.serializers import (TagListSerializerField, TaggitSerializer)
 
 class SmallImageSerializer(serializers.ModelSerializer):
 
@@ -53,10 +54,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
     creator = FeedUserSerializer()
+    tags = TagListSerializerField()
 
     class Meta:
         model = models.Image
@@ -68,6 +70,7 @@ class ImageSerializer(serializers.ModelSerializer):
             'comments',
             'like_count',
             'creator',
+            'tags',
             'created_at'
         )
 
