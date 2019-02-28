f138850166cb1f8781ba80792ba1b8833fb9d980
JaeSukHwang <dajasin245@naver.com>
2019-03-01 01:49:21 +0900

    handle login event

--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -1,12 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
 import "shared/formStyles.scss";
 
 const LoginForm = props => (
     <div className="formComponent">
-        <form className="form">
-            <input type="text" placeholder="사용자 이름" className="textInput" />
-            <input type="password" placeholder="비밀번호" className="textInput" />
+        <form className="form" onSubmit={props.handleSubmit}>
+            <input type="text" placeholder="사용자 이름" className="textInput"
+            onChange={props.handleInputChange} name="username" value={props.usernameValue}/>
+            <input type="password" placeholder="비밀번호" className="textInput" 
+            onChange={props.handleInputChange} name="password" value={props.passwordValue}/>
             <input type="submit" value="로그인" className="button"/>
         </form>
         <span className="divider">또는</span>
@@ -16,5 +19,12 @@ const LoginForm = props => (
         <span className="forgotLink">비밀번호를 잊으셨나요?</span>
     </div>
 );
+
+LoginForm.propTypes = {
+    handleInputChange: PropTypes.func.isRequired,
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleSubmit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
