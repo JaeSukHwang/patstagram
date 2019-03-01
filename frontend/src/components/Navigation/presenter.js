53fe37f5cda1bcc4c565fd7a0902fe4a332c1ea7
JaeSukHwang <dajasin245@naver.com>
2019-03-02 01:09:21 +0900

    navbar-outline

---
+++ frontend/src/components/Navigation/presenter.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Ionicon from "react-ionicons";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import "./styles.scss";
+
+const Navigation = (props) => (
+    <div className="navigation">
+        <div className="inner">
+            <div className="column">
+                <Link to="/">
+                    <img
+                        src={require("images/logo.png")}
+                        className="logo"
+                        alt="Logo"
+                    />
+                </Link>
+            </div>
+            <div className="column">
+                <input type="text" placeholder="Search" className="serachInput"
+                />
+            </div>
+            <div className="column">
+                <div className="navIcon">
+                    <Link to = "/expore">
+                        <Ionicon icon="ios-compass-outline" fontSize="28px" color="black" />
+                    </Link>
+                </div>
+                <div className={StyleSheet.navIcon}>
+                    <Ionicon icon="ios-heart-outline" fontSize="28px" color="black" />
+                </div>
+                <div className="navIcon">
+                    <Link to = "/profile">
+                        <Ionicon icon="ios-person-outline" fontSize="32px" color="black" />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Navigation;
