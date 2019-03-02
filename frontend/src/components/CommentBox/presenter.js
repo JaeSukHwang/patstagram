0d03b05f40372d5e30b9bc3198c0230abc27dc58
JaeSukHwang <dajasin245@naver.com>
2019-03-02 20:29:56 +0900

    commentBox css

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,10 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 import "./styles.scss";
 
 const CommentBox = (props) => (
-    <form>
-        <textarea className="input" placeholder="댓글을 추가하세요..." />
+    <form className="commentBox">
+        <Textarea className="input" placeholder="댓글을 추가하세요..." />
     </form>
 );
 
