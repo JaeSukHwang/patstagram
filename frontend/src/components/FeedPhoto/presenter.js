9d6b680c96bb661dc190e2f8418e9ddf302e98a4
JaeSukHwang <dajasin245@naver.com>
2019-03-02 14:39:59 +0900

    Feed outline

---
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+const FeedPhoto = (props) => {
+    return <div className="feedPhoto">"hello"</div>;
+}
+
+FeedPhoto.propTypes = {
+    creator: PropTypes.shape({
+        profile_image: PropTypes.string,
+    username: PropTypes.string.isRequired
+    }).isRequired,
+    location: PropTypes.string.isRequired,
+    file: PropTypes.string.isRequired,
+    like_count: PropTypes.number.isRequired,
+    caption: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(
+        PropTypes.shape({
+            message: PropTypes.string.isRequired,
+            creator: PropTypes.shape({
+                profile_image: PropTypes.string,
+                username: PropTypes.string.isRequired
+            }).isRequired 
+        })
+    ).isRequired,
+    created_at: PropTypes.string.isRequired
+};
+
+export default FeedPhoto;
