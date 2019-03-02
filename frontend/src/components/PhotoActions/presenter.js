ad6b9fcb157f6fef09baa66d9f158edafeece978
JaeSukHwang <dajasin245@naver.com>
2019-03-02 20:40:44 +0900

    likes and comments css

--- frontend/src/components/PhotoActions/presenter.js
+++ frontend/src/components/PhotoActions/presenter.js
@@ -4,16 +4,16 @@ import Ionicon from "react-ionicons";
 import "./styles.scss"
 
 const PhotoActions = (props) => (
-    <div>
-        <div>
-            <span>
+    <div className="actions">
+        <div className="icons">
+            <span className="icon">
                 <Ionicon icon="ios-heart-outline" fontSize="28px" color="black" />
             </span>
-            <span>
+            <span className="icon">
                 <Ionicon icon="ios-text-outline" fontSize="28px" color="black" />
             </span>
         </div>
-        <span>
+        <span className="likes">
             좋아요 {props.number}개
         </span>
     </div>
