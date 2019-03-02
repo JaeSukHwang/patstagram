30a6b1caa1d3e570254b78af378a1e186c09c4d0
JaeSukHwang <dajasin245@naver.com>
2019-03-02 20:47:56 +0900

    timestamp css added

--- frontend/src/components/TimeStamp/index.js
+++ frontend/src/components/TimeStamp/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./styles.scss";
 
-const TimeStamp = (props) => props.time;
+const TimeStamp = (props) => (<span className="time">{props.time}</span>);
 
-TimeStamp.PropTypes = {
+TimeStamp.propTypes = {
     time: PropTypes.string.isRequired
 };
 
