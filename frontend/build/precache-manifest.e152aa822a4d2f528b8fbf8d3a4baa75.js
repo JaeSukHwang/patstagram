7c585ba07c04b01f0487b511fce5a676217c5216
JaeSukHwang <dajasin245@naver.com>
2019-03-07 11:07:23 +0900

    react to s3

---
+++ frontend/build/precache-manifest.e152aa822a4d2f528b8fbf8d3a4baa75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bd010fa59083b690a2645d65fb8c19f",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/media/logo.4bd010fa.png"
+  },
+  {
+    "revision": "45e6df9a6363101cf9dffcade7cb5bf1",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/media/background.45e6df9a.jpg"
+  },
+  {
+    "revision": "f02d7718fb2e7a53439d4815d57b76eb",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/media/android.f02d7718.png"
+  },
+  {
+    "revision": "3e472b712ddb33e0dbd4",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/js/runtime~main.3e472b71.js"
+  },
+  {
+    "revision": "536b6b1ea73e1d3c5ee0",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/js/main.536b6b1e.chunk.js"
+  },
+  {
+    "revision": "4e90f5706b67f059eb2b",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/js/2.4e90f570.chunk.js"
+  },
+  {
+    "revision": "536b6b1ea73e1d3c5ee0",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/css/main.c814ebbf.chunk.css"
+  },
+  {
+    "revision": "4e90f5706b67f059eb2b",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/static/css/2.3670275d.chunk.css"
+  },
+  {
+    "revision": "4db9c9eeb0459f1c3e1c6426ae483f4e",
+    "url": "https://hwangjaesuk.s3.amazonaws.com/index.html"
+  }
+];
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20c8cb692101beb08590121029aa139b.js"
+  "https://hwangjaesuk.s3.amazonaws.com/precache-manifest.e152aa822a4d2f528b8fbf8d3a4baa75.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://hwangjaesuk.s3.amazonaws.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
